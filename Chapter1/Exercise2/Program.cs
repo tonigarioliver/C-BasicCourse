fbe9569e6769801ee45ad5e80316ba601c17acb1
tonigarioliver <tonietmoscari@gmail.com>
2022-10-11 19:52:15 +0200

    testcommit

--- Chapter1/Exercise2/Program.cs
+++ Chapter1/Exercise2/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Let's see what time it is....");
+Console.WriteLine("Let's see what time it is...?");
 string time = DateTime.Now.ToString();
 Console.WriteLine("The current time is: " + time);
