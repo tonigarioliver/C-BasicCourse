8566a58cb6416d9e9736b9e462a88afebb56a4af
tonigarioliver <tonigarioliver@gmail.com>
2022-10-12 16:40:18 +0100

    Lesson2 started

---
+++ Chapter2/Lessons/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string str = "32";
+Console.WriteLine("The string value is: " + str);
+int intFromString = Convert.ToInt32(str);
+Console.WriteLine("The int value is: " + intFromString);
+
+int? i = null;
+Console.WriteLine(i.HasValue);
