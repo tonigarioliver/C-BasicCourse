fce598f5aa5a44385697bcda8a6cb89c358292fb
tonigarioliver <tonigarioliver@gmail.com>
2022-10-13 18:14:51 +0100

    exercise1 finished

--- Chapter2/Exercise 1/Program.cs
+++ Chapter2/Exercise 1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Car description
+String? name = null;
+String? surname = null;
+int? age = null;
+bool? programer = null;
+Console.WriteLine("Introduce yor person details: ");
+Console.WriteLine("Name: ");
+name = Console.ReadLine();
+Console.WriteLine("Surname: ");
+surname = Console.ReadLine();
+Console.WriteLine("Age: ");
+age = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Is he a programmer? ");
+programer = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("Yor person is: " + name + " " + surname + "is: " + age + "old and is it a programer?: " + programer);
