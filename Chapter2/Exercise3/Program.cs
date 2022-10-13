d9a7d805ded1bc4af81de1c23d77f9f96416b7fc
tonigarioliver <tonigarioliver@gmail.com>
2022-10-13 18:22:03 +0100

    exercise3 lesson2 finished

---
+++ Chapter2/Exercise3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Is older than 18
+int age = 0;
+if (age < 18)
+{
+    Console.WriteLine("He is younger than 18");
+}
+else
+{
+    Console.WriteLine("He is equal or older than 18");
+}
+
+char letter = 'a';
+if (letter.Equals('a'))
+{
+    Console.WriteLine("The letter is equal to A");
+}
+bool fast = false;
+bool sport = false;
+if (fast && sport)
+{
+
+}
+else
+{
+    Console.WriteLine("They are not an athlete");
+}
