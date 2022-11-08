3b2ce012c4054a3121affd79f3e2ed2b76da23d9
tonigarioliver <tonigarioliver@gmail.com>
2022-11-08 13:14:51 +0000

    Exercise chapter5 finished

---
+++ Chapter5/Exercise5/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Exercise1
+Console.WriteLine("Introduce los siguientes datos:");
+Console.WriteLine("Nombre: ");
+String name = Console.ReadLine();
+Console.WriteLine("Email:");
+String email = Console.ReadLine();
+Console.WriteLine("Introduce si o no para cupon descuento:");
+bool cupon = Convert.ToBoolean(Console.ReadLine());
+double price = 10.5;
+double descuento = 10;
+if (cupon == false)
+{
+    Console.WriteLine("Useted no tiene descuento por tanto el precio es de: " + price);
+}
+else
+{
+    price = price * ((100 - descuento) * 0.01);
+    Console.WriteLine("Useted  tiene descuento por tanto el precio es de: " + price);
+}
---
+++ Chapter5/Lesson5/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string hoy = "Lunes";
+
+if (hoy == "Lunes")
+{
+    Console.WriteLine("Hoy es lunes");
+}
+else
+{
+    Console.WriteLine("Hoy no es lunes");
+}
+
+int numsqrt = 100;
+Console.WriteLine(Math.Sqrt(numsqrt));
